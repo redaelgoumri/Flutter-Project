e2aaae77716cdd58fe38a62fceecbebfde93351d
Oussama BITAOUI <oussamabitaa10@gmail.com>
2024-11-14 10:39:28 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/myApp.dart';
+import 'package:test_project/myHome.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+
+
---
+++ lib/registerForm.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/menu.dart';
+
+class RegisterForm extends StatelessWidget {
+   RegisterForm({super.key});
+  TextEditingController name_controller = TextEditingController();
+   TextEditingController email_controller = TextEditingController();
+    TextEditingController password_controller = TextEditingController();
+     TextEditingController confirm_password_controller = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+            Scaffold(
+              
+              appBar: AppBar(
+                title: const Text('Register Form'),
+              ),
+              endDrawer: const Menu() ,
+              body:  Form(
+                child:Column(
+                  children: [
+                    TextFormField(
+                      controller: name_controller ,
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      decoration: InputDecoration(icon: Icon(Icons.person), labelText: 'Name'),
+                    ),
+
+                    SizedBox(height:50.0),
+                       TextFormField(
+                      controller: email_controller ,
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      validator: (value) {
+                        
+                        return '';
+                      },
+                      decoration: InputDecoration(icon: Icon(Icons.mail), labelText: 'Email'),
+                    ),
+                      TextFormField(
+                      controller: password_controller ,
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      decoration: InputDecoration(icon: Icon(Icons.password), labelText: 'Password'),
+                    ),
+                      TextFormField(
+                      controller: confirm_password_controller ,
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      decoration: InputDecoration(icon: Icon(Icons.password), labelText: 'Confirm Password'),
+                    ),
+                    ElevatedButton(onPressed: (){},
+                     child:  Text('Register') )
+                  ],
+                ) 
+                ),
+              
+            );
+  }
+}
